0952c0226cdc8234703e76ee76d72795302ac191
Shahab <shahab.school@hotmail.com>
2022-06-20 22:09:46 -0400

    Updated Link Refference

--- src/App.js
+++ src/App.js
@@ -20,7 +20,9 @@ function App() {
 
 	// Fetch Tasks
 	const fetchTasks = async () => {
-		const res = await fetch("http://localhost:5000/tasks")
+		const res = await fetch(
+			"https://my-json-server.typicode.com/Shab-h/React-Task-Tracker-Json-Server/tasks"
+		)
 		const data = await res.json()
 
 		return data
@@ -28,7 +30,9 @@ function App() {
 
 	// Fetch Task
 	const fetchTask = async (id) => {
-		const res = await fetch(`http://localhost:5000/tasks/${id}`)
+		const res = await fetch(
+			`https://my-json-server.typicode.com/Shab-h/React-Task-Tracker-Json-Server/${id}`
+		)
 		const data = await res.json()
 
 		return data
@@ -37,13 +41,16 @@ function App() {
 	// Add Task
 	const addTask = async (task) => {
 		// --- Add task with Json Server ---
-		const res = await fetch("http://localhost:5000/tasks", {
-			method: "POST",
-			headers: {
-				"Content-type": "application/json",
-			},
-			body: JSON.stringify(task),
-		})
+		const res = await fetch(
+			"https://my-json-server.typicode.com/Shab-h/React-Task-Tracker-Json-Server/tasks",
+			{
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify(task),
+			}
+		)
 
 		const data = await res.json()
 
@@ -57,9 +64,12 @@ function App() {
 
 	// Delete Tassk
 	const deleteTask = async (id) => {
-		await fetch(`http://localhost:5000/tasks/${id}`, {
-			method: "DELETE",
-		})
+		await fetch(
+			`https://my-json-server.typicode.com/Shab-h/React-Task-Tracker-Json-Server/${id}`,
+			{
+				method: "DELETE",
+			}
+		)
 
 		setTasks(tasks.filter((task) => task.id !== id))
 	}
@@ -69,13 +79,16 @@ function App() {
 		const taskToToggle = await fetchTask(id)
 		const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
 
-		const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-			method: "PUT",
-			headers: {
-				"Content-type": "application/json",
-			},
-			body: JSON.stringify(updTask),
-		})
+		const res = await fetch(
+			`https://my-json-server.typicode.com/Shab-h/React-Task-Tracker-Json-Server/${id}`,
+			{
+				method: "PUT",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify(updTask),
+			}
+		)
 
 		const data = await res.json()
 
